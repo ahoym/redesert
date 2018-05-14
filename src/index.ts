7f58cfdc3335a41ee9b441ce8acb9cbc08e9d1bb
Malcolm Ahoy <malcolmahoy@gmail.com>
2018-05-14 19:26:02 -0400

    Also add default entitiesPath

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@ import resourceReducerFactory from './reducer-factories/resource-reducer-factory
 import defaultReducerFactories from './reducer-factories/default-factories';
 import { MakeReducerConfiguration } from './reducer-factories/type-definitions';
 
+const entitiesPath = 'byId';
+
 export const makeResourceReducer = (
   config: MakeReducerConfiguration
-): Function => resourceReducerFactory({ defaultReducerFactories, ...config });
+): Function =>
+  resourceReducerFactory({ defaultReducerFactories, entitiesPath, ...config });
 
 export { resourceReducerFactory };
