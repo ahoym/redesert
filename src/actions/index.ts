5780b101d1b74c4b3f07e153a155c86fc4870d36
Malcolm Ahoy <malcolmahoy@gmail.com>
2018-05-13 21:48:53 -0400

    Add isValidActionTypeHelper for usage in reducers
    This will help reducers determine if the action is relevant to the cases
    it covers. If not, return state

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,3 +1,4 @@
 export { default as API_LIFECYCLE_SUFFIXES } from './api-lifecycle-suffixes';
 export { default as API_ACTION_PREFIXES } from './api-action-prefixes';
+export { default as isValidActionType } from './is-valid-action-type';
 export * from './type-definitions';
---
+++ src/actions/is-valid-action-type.ts
@@ -0,0 +1,13 @@
+import { ALL_API_LIFECYCLE_SUFFIXES } from './api-lifecycle-suffixes';
+
+export default function isValidActionType(
+  reducerNamespace: string,
+  actionType: string
+) {
+  const validPrefix: boolean = actionType.startsWith(reducerNamespace);
+  const validSuffix: boolean = ALL_API_LIFECYCLE_SUFFIXES.some(suffix =>
+    actionType.endsWith(suffix)
+  );
+
+  return validPrefix && validSuffix;
+}
