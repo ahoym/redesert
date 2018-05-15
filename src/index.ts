906e672a6782d6128214ebb93f906cf42df5d972
Malcolm Ahoy <malcolmahoy@gmail.com>
2018-05-15 11:35:36 -0400

    Expose selectorsFactory

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
-import resourceReducerFactory from './reducer-factories/resource-reducer-factory';
 import defaultReducerFactories from './reducer-factories/default-factories';
+import resourceReducerFactory from './reducer-factories/resource-reducer-factory';
+import selectorsFactory from './selector-factories/base-selectors';
 import { DefaultReducerFactories } from './reducer-factories/type-definitions';
 
 const entitiesPath = 'byId';
 
-type Config = {
+type MakeResourceReducerConfig = {
   entitiesPath: string;
   initialState?: any;
   defaultReducerFactories?: DefaultReducerFactories;
   resource: string;
 };
 
-export const makeResourceReducer = (config: Config): Function =>
+export const makeResourceReducer = (
+  config: MakeResourceReducerConfig
+): Function =>
   resourceReducerFactory({
     defaultReducerFactories,
     entitiesPath,
     ...config,
   });
 
-export { resourceReducerFactory };
+type MakeResourceSelectorsConfig = {
+  entitiesPath: string;
+  resource: string;
+};
+export const makeResourceSelectors = (config: MakeResourceSelectorsConfig) =>
+  selectorsFactory({ entitiesPath, ...config });
+
+export { resourceReducerFactory, selectorsFactory };
