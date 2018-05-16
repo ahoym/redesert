05758d007ca1e0317bfd1f3fb2b05bd206997c16
Malcolm Ahoy <malcolmahoy@gmail.com>
2018-05-15 23:57:15 -0400

    Add resourceApiActionTypesFactory
    For making baseApiActionTypes for usage in thunking, sagas, etc.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,4 +1,5 @@
 export { default as API_LIFECYCLE_SUFFIXES } from './api-lifecycle-suffixes';
+export { resourceApiActionTypesFactory } from './api-action-prefixes';
 export { default as API_ACTION_PREFIXES } from './api-action-prefixes';
 export { default as isValidActionType } from './is-valid-action-type';
 export * from './type-definitions';
