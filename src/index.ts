6bd3211438ee7b9fa388b7329d1a85c15d099459
Malcolm Ahoy <colmahoy@gmail.com>
2018-05-15 20:54:09 -0400

    Make selector factories (#10)
    * Further specify types for actual usage
    
    * Remove collectCoverage on regular test runs
    
    While useful, it produces a lot of noise when done on development test
    runs. Can just do it on ci only, or if a developer manually calls the
    test:ci script
    
    * Add selectorsFactory and specs
    
    Dynamically generate selectors for the dynamically generated reducers.
    
    Add the following packages:
    - reselect
    - lodash.capitalize
    - lodash.camelcase
    - @types/reselect
    - @types/lodash.capitalize
    - @types/lodash.camelcase
    
    * Expose selectorsFactory
    
    * Add reselect for devDependencies
    
    * Fix codeclimate duplication complaints
    
    * Codeclimate plz

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import resourceReducerFactory from './reducer-factories/resource-reducer-factory';
 import defaultReducerFactories from './reducer-factories/default-factories';
-import { MakeReducerConfiguration } from './reducer-factories/type-definitions';
+import resourceReducerFactory from './reducer-factories/resource-reducer-factory';
+import selectorsFactory from './selector-factories/base-selectors';
+import { DefaultReducerFactories } from './reducer-factories/type-definitions';
 
 const entitiesPath = 'byId';
 
-export const makeResourceReducer = (
-  config: MakeReducerConfiguration
-): Function =>
-  resourceReducerFactory({ defaultReducerFactories, entitiesPath, ...config });
+type MakeResourceReducerConfig = {
+  entitiesPath: string;
+  initialState?: any;
+  defaultReducerFactories?: DefaultReducerFactories;
+  resource: string;
+};
 
-export { resourceReducerFactory };
+export const makeResourceReducer = (
+  config: MakeResourceReducerConfig
+): Function =>
+  resourceReducerFactory({
+    defaultReducerFactories,
+    entitiesPath,
+    ...config,
+  });
+
+type MakeResourceSelectorsConfig = {
+  entitiesPath: string;
+  resource: string;
+};
+export const makeResourceSelectors = (config: MakeResourceSelectorsConfig) =>
+  selectorsFactory({ entitiesPath, ...config });
+
+export { resourceReducerFactory, selectorsFactory };
---
+++ src/selector-factories/base-selectors.ts
@@ -0,0 +1,80 @@
+import camelCase from 'lodash.camelcase';
+import capitalize from 'lodash.capitalize';
+import { createSelector } from 'reselect';
+import { ReduxSliceState } from './../reducer-factories/type-definitions';
+
+const pascalize = (string: string) => capitalize(camelCase(string));
+
+export type SelectorsFactoryConfig = {
+  entitiesPath: string;
+  resource: string;
+};
+type State = {
+  [resource_name: string]: any;
+};
+type Options = {
+  id: string;
+};
+export type SelectorFns = {
+  [selector_name: string]: Function;
+};
+
+function selectorsFactory({
+  entitiesPath,
+  resource,
+}: SelectorsFactoryConfig): SelectorFns {
+  const resourceName = pascalize(resource);
+
+  const getEntities = (state: State) => state[resource][entitiesPath];
+  const getId = (_state: State, { id }: Options) => id;
+
+  const getEntityById = createSelector(
+    getEntities,
+    getId,
+    (entities, id) => entities[id]
+  );
+
+  const getFirstEntity = createSelector(getEntities, entities => {
+    const firstKey = Object.keys(entities)[0];
+    return entities[firstKey];
+  });
+
+  const getSliceErrors = (state: ReduxSliceState) => state[resource].errors;
+
+  const getEntityErrorsById = createSelector(
+    getEntityById,
+    entity => entity.errors
+  );
+
+  const areEntitiesFetching = (state: ReduxSliceState) =>
+    !!state[resource].isFetching;
+
+  const isEntityFetching = createSelector(
+    getEntityById,
+    entity => !!entity.isFetching
+  );
+
+  const isEntityUpdating = createSelector(
+    getEntityById,
+    entity => !!entity.isUpdating
+  );
+
+  const isEntityRemoving = createSelector(
+    getEntityById,
+    entity => !!entity.isRemoving
+  );
+
+  return {
+    [`get${resourceName}Entities`]: getEntities,
+    [`get${resourceName}ById`]: getEntityById,
+    [`getCurrent${resourceName}`]: getFirstEntity,
+    [`get${resourceName}Errors`]: getSliceErrors,
+    [`get${resourceName}ErrorsById`]: getEntityErrorsById,
+    [`getAre${resourceName}EntitiesFetching`]: areEntitiesFetching,
+    [`getIs${resourceName}Fetching`]: isEntityFetching,
+    [`getIs${resourceName}Updating`]: isEntityUpdating,
+    [`getIs${resourceName}Removing`]: isEntityRemoving,
+  };
+}
+
+export default selectorsFactory;
